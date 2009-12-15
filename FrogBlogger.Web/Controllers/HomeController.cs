92eac0eae9a04160c3d2bf7c6d0326708f2e7adb
Sean Fao <sean.fao@gmail.com>
2009-12-14 23:09:12 -0500

    Started adding blog stats to the home page. Needs styling.

--- FrogBlogger.Web/Controllers/HomeController.cs
+++ FrogBlogger.Web/Controllers/HomeController.cs
@@ -12,8 +12,11 @@ namespace FrogBlogger.Web.Controllers
     [HandleError]
     public class HomeController : Controller
     {
+        private static Guid _blogId = new Guid("7F2C3923-5FC8-4A8C-8ABF-21DD40F16C6C"); // TODO: Place this with the current BlogId
+
         public ActionResult Index()
         {
+            int count;
             int maxRecords = 10; // TODO: Replace the magic number 10 on this line with a user determined value
             HomeViewModel model;
             List<BlogPost> posts;
@@ -22,8 +25,14 @@ namespace FrogBlogger.Web.Controllers
             using (IDataRepository<BlogPost> repository = new DataRepository<BlogPost>())
             {
                 posts = (from m in repository.Fetch()
+                         where m.BlogId == _blogId
                          orderby m.PostedDate descending
                          select m).Take(maxRecords).ToList();
+
+                // Get the total blog post count
+                count = (from m in repository.Fetch()
+                         where m.BlogId == _blogId
+                         select m).Count();
 
                 // Determine the comment count for each blog post
                 foreach (BlogPost post in posts)
@@ -31,7 +40,7 @@ namespace FrogBlogger.Web.Controllers
                     blogPostCommentCount.Add(post.BlogPostId, (uint)post.UserComments.Count);
                 }
 
-                model = new HomeViewModel(posts, blogPostCommentCount);
+                model = new HomeViewModel(posts, blogPostCommentCount, (uint)count);
             }
 
             return View(model);
