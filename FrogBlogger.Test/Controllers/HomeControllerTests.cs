658810af68919b4d7de6e6874ca2ad12819c9307
Sean Fao <sean.fao@gmail.com>
2009-12-24 16:14:29 -0500

    Started adding unit tests (something I should have done a long time ago).

---
+++ FrogBlogger.Test/Controllers/HomeControllerTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FrogBlogger.Web.Controllers;
+using System.Web.Mvc;
+using FrogBlogger.Web.Models;
+using FrogBlogger.Dal;
+
+namespace FrogBlogger.Test.Controllers
+{
+    /// <summary>
+    /// Contains test methods for the home controller
+    /// </summary>
+    [TestClass]
+    public class HomeControllerTests
+    {
+        #region Fields
+
+        /// <summary>
+        /// Stores the HomeController
+        /// </summary>
+        private HomeController _controller = new HomeController();
+
+        /// <summary>
+        /// Stores the result from an action method
+        /// </summary>
+        private ViewResult _viewResult;
+
+        /// <summary>
+        /// Stores the ViewModel returned from an action method
+        /// </summary>
+        private HomeViewModel _viewModel;
+
+        #endregion
+
+        #region Test Methods
+
+        /// <summary>
+        /// Initializes the test class
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            _viewResult = _controller.Index() as ViewResult;
+            _viewModel = _viewResult.ViewData.Model as HomeViewModel;
+        }
+
+        /// <summary>
+        /// Verifies that a blog contains blog posts
+        /// </summary>
+        [TestMethod]
+        public void ContainsBlogPosts()
+        {
+            Assert.IsTrue(_viewModel.BlogPosts.Count > 0);
+        }
+
+        /// <summary>
+        /// Verifies that the maximum blog post count does not exceed the threshold
+        /// </summary>
+        [TestMethod]
+        public void BlogPostCount()
+        {
+            Assert.IsTrue(_viewModel.BlogPostCount <= 10); // TODO: Threshold should be user configurable
+            Assert.AreEqual<int>(_viewModel.BlogPostCount, _viewModel.BlogPosts.Count);
+        }
+
+        /// <summary>
+        /// Verifies that the msot recent blog posts fall at the top of the list
+        /// </summary>
+        [TestMethod]
+        public void BlogOrderDescending()
+        {
+            DateTime previousDate = DateTime.MaxValue;
+
+            foreach (BlogPost post in _viewModel.BlogPosts)
+            {
+                Assert.IsTrue(post.PostedDate <= previousDate);
+
+                previousDate = post.PostedDate.Value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ FrogBlogger.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FrogBlogger.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FrogBlogger.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM componenets.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07900235-7ec1-4c7f-9401-a959a187f509")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
