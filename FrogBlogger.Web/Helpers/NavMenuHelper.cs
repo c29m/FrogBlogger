cf953714514833f3715beb442923915b1b09b568
c29m <c29.hunter@gmail.com>
2009-12-27 15:33:12 -0500

    Implemented code to show the Admin menu item only if the user is an Admin for the current blog.

---
+++ FrogBlogger.Web/Helpers/NavMenuHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Security.Principal;
+using FrogBlogger.Dal;
+using FrogBlogger.Dal.Interfaces;
+using FrogBlogger.Web.Helpers;
+
+namespace System.Web.Mvc
+{
+    public static class NavMenuHelper
+    {
+        /// <summary>
+        /// Renders an admin menu item on the navbar if the user is an admin
+        /// </summary>
+        /// <param name="helper">Extends the HtmlHelper class</param>
+        /// <param name="user">The current user</param>
+        /// <returns>Renders a list item to add to the menu bar for an admin, if the user is a member
+        /// of the admin role. Otherwise, an emptyh string is returned.</returns>
+        public static string AdminMenuItem(this HtmlHelper helper, IPrincipal user)
+        {
+            string menuItem = string.Empty;
+
+            if (user.IsInRole(FrogBlogger.Web.Helpers.Roles.Admin))
+            {
+                Guid blogId = BlogUtility.GetBlogId();
+
+                using (IDataRepository<Author> repository = new DataRepository<Author>())
+                {
+                    if (repository.Fetch(a => a.BlogId == blogId).Any())
+                    {
+                        menuItem = "<li><a href=\"/Admin/\">Admin</a></li>";
+                    }
+                }
+            }
+
+            return menuItem;
+        }
+    }
+}
--- FrogBlogger.Web/Views/Shared/MenuUserControl.ascx
+++ FrogBlogger.Web/Views/Shared/MenuUserControl.ascx
@@ -2,7 +2,7 @@
 <div id="menu">
     <ul>              
         <li class="first"><%= Html.ActionLink("Home", "Index", "Home")%></li>
-        <li><%= Html.ActionLink("Admin", "Index", "Admin")%></li>
+        <%= Html.AdminMenuItem(Context.User) %>
         <li><a href="#">Syndication <img src="/Content/Images/xml.gif" alt="XML" /></a></li>
 	    <li><a href="#">Contact Us</a></li>
         <li><%= Html.ActionLink("About", "About", "Home")%></li>
