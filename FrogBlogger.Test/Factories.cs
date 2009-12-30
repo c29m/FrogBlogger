134f58ea0214343a12e433165bb5b2f2b3ac3845
Sean Fao <sean.fao@gmail.com>
2009-12-30 11:53:43 -0500

    Continued progress on the NavMenuHelper tests.

---
+++ FrogBlogger.Test/Factories.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Rhino.Mocks;
+
+namespace FrogBlogger.Test
+{
+    /// <summary>
+    /// Contains factory methods for initializing common mocked objects
+    /// </summary>
+    internal static class Factories
+    {
+        /// <summary>
+        /// Gets a mocked HtmlHelper object
+        /// </summary>
+        /// <returns>An HtmlHelper object that can be used for testing</returns>
+        internal static HtmlHelper InitializeHtmlHelper()
+        {
+            MockRepository mocks = new MockRepository();
+            ControllerContext context = mocks.StrictMock<ControllerContext>(
+                mocks.StrictMock<HttpContextBase>(),
+                new RouteData(),
+                mocks.StrictMock<ControllerBase>());
+            ViewContext viewContext = mocks.StrictMock<ViewContext>(context, mocks.StrictMock<IView>(), new ViewDataDictionary(), new TempDataDictionary());
+            HtmlHelper helper = mocks.StrictMock<HtmlHelper>(viewContext, mocks.StrictMock<IViewDataContainer>());
+
+            return helper;
+        }
+    }
+}
