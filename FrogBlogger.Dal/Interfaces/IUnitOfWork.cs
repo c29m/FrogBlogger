ec2b85d5e6ff9bb2be9d968de743537a9c41fb29
Sean Fao <sean.fao@gmail.com>
2010-01-15 18:36:57 -0500

    Created an IObjectContext and an ObjectContextAdapter to help decouple the DAL logic from Entity Framework and to facilitate better unit testing capability.

---
+++ FrogBlogger.Dal/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrogBlogger.Dal.Interfaces
+{
+    /// <summary>
+    /// Defines the interface for a unit of work
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Saves changes in the current context
+        /// </summary>
+        void SaveChanges();
+    }
+}
---
+++ FrogBlogger.Dal/ObjectContextAdapter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data.Objects;
+
+namespace FrogBlogger.Dal
+{
+    /// <summary>
+    /// Creates an adapter for the Entity Framework ObjectContext type
+    /// </summary>
+    public class ObjectContextAdapter : IObjectContext
+    {
+        #region Fields
+
+        /// <summary>
+        /// An Entity Framework ObjectContext
+        /// </summary>
+        private readonly ObjectContext _context;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Prevents the default instance of the ObjectContextAdapter class from being initialized
+        /// </summary>
+        private ObjectContextAdapter()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ObjectContextAdapter class
+        /// </summary>
+        /// <param name="context">Entity Framework context</param>
+        public ObjectContextAdapter(ObjectContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Initializes an ObjectSet that is used to perform create, read, update, and delete operations. 
+        /// </summary>
+        /// <typeparam name="T">The entity type</typeparam>
+        /// <returns>The ObjectSet to query against</returns>
+        public ObjectSet<T> CreateObjectSet<T>() where T : class
+        {
+            return _context.CreateObjectSet<T>();
+        }
+
+        /// <summary>
+        /// Persists all updates to the data source and resets change tracking in the object context.
+        /// </summary>
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Disposes all resources used by the FrogBlogger.Dal.ObjectContextAdapter
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        /// <summary>
+        /// Disposes all resources used by the FrogBlogger.Dal.ObjectContextAdapter
+        /// </summary>
+        /// <param name="disposing">Indicates whether or not the object is disposing of managed resources</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+
+        #endregion
+    }
+}
